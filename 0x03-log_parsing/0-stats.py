110a42c92a8725ad645e6384d88a03e083363c12
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-19 06:22:25 -0700

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,9 +5,11 @@ import re
 
 
 def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
-    fp = (
+    """parse sections of a line from an HTTP request log,
+    and it returns a dictionary containing specific pieces 
+    of information extracted from that log line.
+    """
+    pa = (
         r'\s*(?P<ip>\S+)\s*',
         r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
         r'\s*"(?P<request>[^"]*)"\s*',
@@ -18,7 +20,7 @@ def extract_input(input_line):
         'status_code': 0,
         'file_size': 0,
     }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(pa[0], pa[1], pa[2], pa[3], pa[4])
     resp_match = re.fullmatch(log_fmt, input_line)
     if resp_match is not None:
         status_code = resp_match.group('status_code')
@@ -29,8 +31,7 @@ def extract_input(input_line):
 
 
 def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
-    '''
+    """Prints the accumulated statistics of the HTTP request log."""
     print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
         num = status_codes_stats.get(status_code, 0)
@@ -39,14 +40,9 @@ def print_statistics(total_file_size, status_codes_stats):
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
-
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
-
-    Returns:
-        int: The new total file size.
-    '''
+    """update and maintain metrics based on the information extracted 
+    from an HTTP request log line.
+    """
     line_info = extract_input(line)
     status_code = line_info.get('status_code', '0')
     if status_code in status_codes_stats.keys():
@@ -55,11 +51,10 @@ def update_metrics(line, total_file_size, status_codes_stats):
 
 
 def run():
-    '''Starts the log parser.
-    '''
+    """a function processes lines from an HTTP request log."""
     line_num = 0
     total_file_size = 0
-    status_codes_stats = {
+    status_counts = {
         '200': 0,
         '301': 0,
         '400': 0,
@@ -75,13 +70,14 @@ def run():
             total_file_size = update_metrics(
                 line,
                 total_file_size,
-                status_codes_stats,
+                status_counts,
             )
             line_num += 1
             if line_num % 10 == 0:
-                print_statistics(total_file_size, status_codes_stats)
+                print_statistics(total_file_size, status_counts)
     except (KeyboardInterrupt, EOFError):
-        print_statistics(total_file_size, status_codes_stats)
+        print_statistics(total_file_size, status_counts)
+
 
 if __name__ == '__main__':
     run()
